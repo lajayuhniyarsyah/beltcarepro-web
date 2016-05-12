b28a5f9e1d22a56809c4cff5dfeac57505d7b815
Mohammadrezafah <mrfahlepi@gmail.com>
2016-05-11 21:43:54 -0700

    setting database , static file

--- beltcarepro/beltcarepro/settings.py
+++ beltcarepro/beltcarepro/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'beltcarepro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template/'),os.path.join(BASE_DIR, 'base/template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'beltcarepro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'beltcarepro-1',
+        'USER': 'reza_erp',
+        'PASSWORD': 'supra',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
+
 
 
 # Password validation
@@ -118,4 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static_admin/")
