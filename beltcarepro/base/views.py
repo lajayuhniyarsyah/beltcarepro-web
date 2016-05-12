611f1ef0b0d330c98d2646ae1350238cf883d072
Mohammadrezafah <mrfahlepi@gmail.com>
2016-05-11 21:32:15 -0700

    direct & url

--- beltcarepro/base/views.py
+++ beltcarepro/base/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+import json
 
 def index(request):
-	return HttpResponse("Hello, Welcome in Beltcarepro 4.0")
+	context = {}
+	template = "index.html"
+	return render (request,template,context)
+
+
+def dashboard(request):
+	context = {}
+	template = "content/dashboard.html"
+	return render (request,template,context)
+
+def data_conveyor(request):
+	context = {}
+	template = "content/data_conveyor.html"
+	return render (request,template,context)
+
+def data_customer(request):
+	context = {"range":range(100)}
+	template = "content/data_customer.html"
+	return render (request,template,context)
+
+def data_site(request):
+	context = {}
+	template = "content/data_site.html"
+	return render (request,template,context)
+
+def detail_conveyor_condition(request):
+	context = {"part":["Belt Conveyor","SECTION SPLICE BELT","Pulley","ROLLER","PRIMARY Cleaners","Secondary Cleaners"]}
+	template = "content/detail_conveyor_condition.html"
+	return render (request,template,context)
+
+def login(request):
+
+	if request.method == 'POST':   
+		username = request.POST['username']
+  		password = request.POST['passwd']                                                                                                                                                                                                        
+		login_form =  authenticate(username=username, password=password)
+		response_data = {}                                                                              
+		if login_form is not None:
+			if login_form.is_active:
+				response_data['accessGranted'] = 'Success!'
+				response_data['result'] = 'Success!'
+				response_data['message'] = 'You"re logged in' 		
+		else:
+			response_data['result'] = 'failed'
+			response_data['message'] = 'You messed up'   
+		print response_data['result'],"<<<<<<<<<<<<<<<"
+		return HttpResponse(json.dumps(response_data), content_type="application/json")  
