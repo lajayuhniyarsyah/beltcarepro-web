41f4f5a86a81ce6aeb556ea1b7c62c29d75f09e7
ikhsan07 <ikhsan.madrid@gmail.com>
2016-05-12 23:44:32 -0700

    modified model,views,setting,url

--- beltcarepro/beltcarepro/urls.py
+++ beltcarepro/beltcarepro/urls.py
@@ -13,10 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url,patterns
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'base/', include('base.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$','base.views.index'),
+    url(r'^dashboard/','base.views.dashboard'),
+    url(r'^data_conveyor/','base.views.data_conveyor'),
+    url(r'^data_customer/','base.views.data_customer'),
+    url(r'^view_customer/','base.views.view_customer'),
+    url(r'^add_customer/','base.views.add_customer'),
+    url(r'^data_site/','base.views.data_site'),
+    url(r'^view_site/','base.views.view_site'),
+    url(r'^add_customer_site/','base.views.add_customer_site'),
+    url(r'^detail_conveyor_condition/','base.views.detail_conveyor_condition'),
+     url(r'^login/','base.views.login'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
